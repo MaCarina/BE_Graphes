1cf57cbd925f4665792c7f4621340d5bc649933c
Carina Malleville <carina.ma@live.fr>
2021-04-14 20:43:09 +0200

    Ajout de la classe Label

---
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -0,0 +1,41 @@
+package org.insa.graphs.algorithm.shortestpath;
+
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Node;
+
+public class Label implements Comparable<Label>{
+	
+	public Node sommet_courant;
+	
+	public boolean marque;
+	
+	public float cout;
+	
+	public Node pere;
+	public Arc arc_pere;
+	
+	public Label(Node node) {
+		this.sommet_courant=node;
+		this.marque=false;
+		this.cout=Float.MAX_VALUE;
+		this.pere=null;
+		this.arc_pere=null;
+	}
+	
+	public Label(Node node,boolean marque,float cout,Arc arc_pere) {
+		this.sommet_courant=node;
+		this.marque=marque;
+		this.cout=cout;
+		this.pere=null;
+		this.arc_pere=arc_pere;
+	}
+	
+	public float getCost() {
+        return this.cout;
+    }
+	
+	public int compareTo(Label other) {
+        return Float.compare(cout, other.cout);
+    }
+
+}
