7ce456fedb5139015c26311e146f597ad9715fe6
Carina Malleville <carina.ma@live.fr>
2021-05-09 14:33:59 +0200

    Ajout du DijkstraTest

---
+++ be-graphes-algos/src/test/java/org/insa/graphs/algorithm/utils/DijkstraTest.java
@@ -0,0 +1,18 @@
+package org.insa.graphs.algorithm.utils;
+
+import static org.junit.Assert.assertEquals;
+import org.insa.graphs.model.Node;
+
+public class DijkstraTest {
+
+	//scénarios
+	private static Carte carte;
+	
+	private static float cout;
+	
+	private static Node origine;
+	
+	private static Node destination;
+    
+    
+}
