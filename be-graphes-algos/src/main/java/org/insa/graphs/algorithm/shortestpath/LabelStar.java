4363499f886c009696b1c9091e207725edb3298c
Carina Malleville <carina.ma@live.fr>
2021-05-11 17:51:01 +0200

    Insertion de la classe LabelStar

---
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/LabelStar.java
@@ -0,0 +1,23 @@
+package org.insa.graphs.algorithm.shortestpath;
+
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Node;
+
+public class LabelStar extends Label {
+	
+	private float val_estime;
+	
+	public LabelStar(Node node) {
+		super(node);
+	}
+	
+	public LabelStar(Node node,boolean marque,float cout, Arc arc_pere,float val_estime) {
+		super(node,marque,cout,arc_pere);
+		this.val_estime=val_estime;
+	}
+	
+	public float getTotalCost() {
+		return this.val_estime+this.cout;
+	}
+
+}
