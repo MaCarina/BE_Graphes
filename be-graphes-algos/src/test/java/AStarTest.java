f1d5258a757d2d6c26ab34cb140d8f570e2d0b6b
Carina Malleville <carina.ma@live.fr>
2021-05-21 14:42:24 +0200

    Codage des tests de AStarAlgorithm

---
+++ be-graphes-algos/src/test/java/AStarTest.java
@@ -0,0 +1,96 @@
+import static org.junit.Assert.*;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+
+import org.insa.graphs.algorithm.AbstractSolution;
+import org.insa.graphs.algorithm.ArcInspectorFactory;
+import org.insa.graphs.algorithm.shortestpath.BellmanFordAlgorithm;
+import org.insa.graphs.algorithm.shortestpath.AStarAlgorithm;
+import org.insa.graphs.algorithm.shortestpath.ShortestPathData;
+import org.insa.graphs.algorithm.shortestpath.ShortestPathSolution;
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Node;
+import org.insa.graphs.model.Path;
+import org.insa.graphs.model.io.BinaryGraphReader;
+import org.insa.graphs.model.io.GraphReader;
+import org.junit.Test;
+
+public class AStarTest {
+
+	private static Path oneNodePath;
+    @Test
+    public void testCheminSimple() throws Exception {
+    	
+    	String nom_carte = "C:/Users/Carin/BE_Graphes/Maps/carre.mapgr";
+    	GraphReader reader = new BinaryGraphReader(new DataInputStream(new BufferedInputStream(new FileInputStream(nom_carte))));
+    	Graph graph = reader.read();
+    	
+    	float cout = 0;
+    	for (int i=5;i<9;i++) {
+    		for (Arc arc : graph.getNodes().get(i).getSuccessors() ) {
+    			if(arc.getDestination() == graph.getNodes().get(i+1)) {
+    				cout = cout + arc.getLength();
+    				break;
+    			}
+    		}
+    	}
+    	Node origine = graph.getNodes().get(5);
+    	Node destination = graph.getNodes().get(9);
+    	ShortestPathData Data = new ShortestPathData(graph,origine,destination,ArcInspectorFactory.getAllFilters().get(0));
+        AStarAlgorithm d = new AStarAlgorithm(Data);
+        ShortestPathSolution sd = d.doRun();
+    	
+        assertTrue(Math.abs(cout-sd.getPath().getLength())<0.01);
+    }
+    
+    @Test
+	 public void testOneNode() throws Exception {
+		
+		String nom_carte = "C:/Users/Carin/BE_Graphes/Maps/carre.mapgr";
+		BinaryGraphReader reader = new BinaryGraphReader(new DataInputStream(new BufferedInputStream(new FileInputStream(nom_carte))));
+    	Graph graph = reader.read();
+    	
+    	Node node = graph.getNodes().get(0);
+    	oneNodePath = new Path(graph,node);
+    	ShortestPathData Data = new ShortestPathData(graph,node,node,ArcInspectorFactory.getAllFilters().get(0));
+        AStarAlgorithm d = new AStarAlgorithm(Data);
+        ShortestPathSolution sd = d.doRun();
+	    assertTrue(Math.abs(oneNodePath.getLength()-sd.getPath().getLength())<0.01);
+	}
+	
+    @Test
+    public void testMediumPath() throws Exception{
+    	
+    	String nom_carte = "C:/Users/Carin/BE_Graphes/Maps/haute-garonne.mapgr";
+		BinaryGraphReader reader = new BinaryGraphReader(new DataInputStream(new BufferedInputStream(new FileInputStream(nom_carte))));
+    	Graph graph = reader.read();
+    	
+    	Node origine = graph.getNodes().get(105411);
+    	Node destination = graph.getNodes().get(157779);
+    	ShortestPathData Data = new ShortestPathData(graph,origine,destination,ArcInspectorFactory.getAllFilters().get(0));
+        AStarAlgorithm d = new AStarAlgorithm(Data);
+        ShortestPathSolution sd = d.doRun();
+        BellmanFordAlgorithm b = new BellmanFordAlgorithm(Data);
+        ShortestPathSolution sb = b.doRun();
+	    assertTrue(Math.abs(sb.getPath().getLength()-sd.getPath().getLength())<0.01);
+    }
+
+    @Test
+    public void testConnexite() throws Exception{
+    	
+    	String nom_carte = "C:/Users/Carin/BE_Graphes/Maps/french-polynesia.mapgr";
+		BinaryGraphReader reader = new BinaryGraphReader(new DataInputStream(new BufferedInputStream(new FileInputStream(nom_carte))));
+    	Graph graph = reader.read();
+    	
+    	Node origine = graph.getNodes().get(6564);
+    	Node destination = graph.getNodes().get(3638);
+    	ShortestPathData Data = new ShortestPathData(graph,origine,destination,ArcInspectorFactory.getAllFilters().get(0));
+        AStarAlgorithm d = new AStarAlgorithm(Data);
+        ShortestPathSolution sd = d.doRun();
+    	assertEquals(sd.getStatus(),AbstractSolution.Status.INFEASIBLE);
+    }
+
+}
